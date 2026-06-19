fe7d38ef32c6b3820b2a4f1196cf8a12c259a87d
Aditya.R <Aditya.R@infotechtion.com>
2026-06-19 16:12:50 +0530

    Fix: Added pub get step to build orchestration script

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# 1. Clone the stable Flutter SDK branch into the build machine environment
+# 1. Clone the stable Flutter SDK branch cleanly
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 
-# 2. Add the freshly cloned Flutter directory paths to the system environment
+# 2. Add Flutter to the path variables
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. Enable Web compilation tracking flags
+# 3. Enable Web configuration tracking
 flutter config --enable-web
 
-# 4. Compile the application using CanvasKit paths natively
+# 4. FETCH DEPENDENCIES (This fixes error 64!)
+flutter pub get
+
+# 5. Compile the web architecture
 flutter build web --release --web-renderer canvaskit
