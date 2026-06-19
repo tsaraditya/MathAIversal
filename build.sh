78a6922d5d381c58b1db8859ef41a3339f247356
Aditya.R <Aditya.R@infotechtion.com>
2026-06-19 16:09:37 +0530

    Fix: Added automated build script for custom linux machine pipelines

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-rm -rf build public
-if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable --depth 1
-fi
+
+# 1. Clone the stable Flutter SDK branch into the build machine environment
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+# 2. Add the freshly cloned Flutter directory paths to the system environment
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# 3. Enable Web compilation tracking flags
 flutter config --enable-web
-flutter pub get
-flutter build web --release --base-href /
-mkdir -p public
-cp -r build/web/* public/
+
+# 4. Compile the application using CanvasKit paths natively
+flutter build web --release --web-renderer canvaskit
